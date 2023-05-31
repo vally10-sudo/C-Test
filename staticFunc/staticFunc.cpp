b793dbae43e146cdd3580119f60f3c1a3864d329
vally10-sudo <66656373+vally10-sudo@users.noreply.github.com>
2023-06-01 05:45:12 +0800

    static
    修饰成员函数，实现静态成员变量的管理

---
+++ staticFunc/staticFunc.cpp
@@ -0,0 +1,41 @@
+﻿#include<iostream>
+#include<math.h>
+using namespace std;
+class Point  //定义坐标类Point
+{
+public:
+	Point(float x, float y);
+	~Point();
+	static float getLen(Point& p1, Point& p2);
+	//声明静态成员函数
+	static float _len;
+private:
+	float _x;
+	float _y;
+};
+
+float Point::_len = 0;
+Point::Point(float x = 0, float y = 0) :_x(x), _y(y)  //类外实现有参构造函数
+{
+	cout << "初始化坐标点" << endl;
+
+}
+
+Point::~Point() {
+}
+
+float Point::getLen(Point& p1, Point& p2)
+{
+	float x = abs(p1._x - p2._x);
+	float y = abs(p1._y - p2._y);
+	_len = sqrtf(x * x + y * y);
+	return _len;
+}
+
+int main()
+{
+	Point p1(1, 2);
+	Point p2(6, 8);
+	cout << Point::getLen(p1, p2) << endl;
+	return 0;
+}
