44c59bfb8331a0481fdf05c5f7d5538b3009b578
vally10-sudo <66656373+vally10-sudo@users.noreply.github.com>
2023-06-01 18:34:04 +0800

    友元函数
    友元函数

---
+++ friendFunc/friendFunc.cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+using namespace std;
+class Circle
+{
+	friend void getArea(Circle& circle);
+	//声明普通函数getArea()为友元函数
+private:
+	float _radius;
+	const float PI = 3.14;
+public:
+	Circle(float radius);
+	~Circle();
+};
+
+Circle::Circle(float radius = 0) :_radius(radius)
+{
+	cout << "初始化圆的半径为：" << _radius << endl;
+}
+Circle::~Circle() {}
+void getArea(Circle & circle) {
+		//访问类中的成员变量
+	cout << "yuande banjingshi :" << circle._radius << endl;
+	cout << "yuan的面积是" << circle.PI * circle._radius * circle._radius << endl;
+	cout << "youyuan hanshu xiugai banjing:" << endl;
+	circle._radius = 1;
+	cout << "yuande banjingshi:" << circle._radius << endl;
+}
+int main()
+{
+	Circle circle(10);
+	getArea(circle);
+	return 0;
+}
